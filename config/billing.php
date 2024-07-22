6129fe9d6ea94a7bfe6359f3a79bc21aa7f08d03
nalzai35 <zainal.jelek@yahoo.com>
2024-07-22 18:37:58 +0700

    fix deprecate for php 8.1

--- config/billing.php
+++ config/billing.php
@@ -17,7 +17,7 @@ return [
     /**
      * Redeemable packages
      */
-    'redeemable_packages'                   => explode(',', env('REFERRAL_MONTHS_REDEEMABLE_PACKAGE')),
+    'redeemable_packages'                   => explode(',', env('REFERRAL_MONTHS_REDEEMABLE_PACKAGE', '')),
 
     /**
      * Limit for adding payment methods per day for same user
